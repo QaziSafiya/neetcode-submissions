cfacecf19291568653c1d2d21b4d6b3acfd6acc3
Safiya Siddiqui <143307549+QaziSafiya@users.noreply.github.com>
2026-05-06 14:37:36 +0530

    Add: height-checker - submission-1

---
+++ Data Structures & Algorithms/height-checker/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int count[101]={};
+        for(auto&it:heights){
+            count[it]++;
+        }
+        vector<int>expected;
+        for(int h=1;h<=100;h++){
+int c=count[h];
+for(int i=0;i<c;i++){
+    expected.push_back(h);
+}
+        }
+        int res = 0;
+for (int i = 0; i < heights.size(); i++) {
+    if (heights[i] != expected[i]) {
+        res++;
+    }
+}
+
+return res;
+    }
+};
