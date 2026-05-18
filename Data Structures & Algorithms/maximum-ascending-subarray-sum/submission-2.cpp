29f64f395788a0c318611f29447ac1057fc5fe0f
Safiya Siddiqui <143307549+QaziSafiya@users.noreply.github.com>
2026-05-18 20:46:51 +0530

    Add: maximum-ascending-subarray-sum - submission-2

---
+++ Data Structures & Algorithms/maximum-ascending-subarray-sum/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+    int maxAscendingSum(vector<int>& nums) {
+        int maxsum = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] <= nums[i - 1]) {
+                sum = 0;
+            }
+            sum = sum + nums[i];
+            maxsum = max(maxsum, sum);
+        }
+
+        return maxsum;
+    }
+};
