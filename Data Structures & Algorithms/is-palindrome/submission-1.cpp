68af31ee81bf2acf0c9f1d0e436d3586102a840b
Safiya Siddiqui <143307549+QaziSafiya@users.noreply.github.com>
2026-07-28 20:04:32 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str="";
+
+for(auto&ch:s){
+    if (isalnum(ch))
+                str += tolower(ch);
+}
+
+string rev=str;
+reverse(rev.begin(),rev.end());
+return rev==str;
+    }
+};
